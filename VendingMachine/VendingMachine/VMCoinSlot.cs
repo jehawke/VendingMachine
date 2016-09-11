d82669ed11b2facfedfd8ac6113bd0a886eb89de
jhawke <jeff.hawke@outlook.com>
2016-09-10 21:51:18 -0400

    Starting to write tests for the coin slot.

---
+++ VendingMachine/VendingMachine/VMCoinSlot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class VMCoinSlot
+    {
+        public bool bItemWasInserted = false;
+
+        public void sendInsertedItemToValidator()
+        {
+            bItemWasInserted = true;
+        }
+    }
+}
---
+++ VendingMachine/VendingMachineTests/VMCoinSlotTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using VendingMachine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class VMCoinSlotTest
+    {
+        [TestMethod]
+        public void TestCoinSlotAcceptsItems()
+        {
+            VMCoinSlot coinSlot = new VMCoinSlot();
+            Assert.IsFalse(coinSlot.bItemWasInserted);
+            coinSlot.sendInsertedItemToValidator();
+            Assert.IsTrue(coinSlot.bItemWasInserted);
+        }
+    }
+}
