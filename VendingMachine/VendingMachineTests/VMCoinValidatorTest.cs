9270386bb01586d975ebfad0bd4c0a3ba1903ba9
jhawke <jeff.hawke@outlook.com>
2016-09-11 19:11:22 -0400

    Started work on Coin Validator, writing first test

---
+++ VendingMachine/VendingMachineTests/VMCoinValidatorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachine;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class VMCoinValidatorTest
+    {
+        [TestMethod]
+        public void TestCoinValidatorReturnsTrueWhenAValidCoinIsPassedIn()
+        {
+            VMCoinValidator testValidator = new VMCoinValidator();
+            Assert.IsTrue(testValidator.ValidateCoin());
+        }
+    }
+}
