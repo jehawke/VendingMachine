3d45179744f6a2e5155f80f1cba36de8e583b011
jhawke <jeff.hawke@outlook.com>
2016-09-14 10:20:11 -0400

    Refactored VmFoodDispenser and wrote associated tests so that it holds chips and candy with the soda, and looks more like a real vending machine

--- VendingMachine/VendingMachine/VmFoodDispenser.cs
+++ VendingMachine/VendingMachine/VmFoodDispenser.cs
@@ -8,18 +8,54 @@ namespace VendingMachine
 {
     public class VmFoodDispenser
     {
-        public List<string> Inventory = new List<string>();
+        private const string SODA_STRING = "Soda";
+        private const string CHIPS_STRING = "Chips";
+        private const string CANDY_STRING  = "Candy";
+
+        private const int NUM_ITEMS_ADDED_IN_RESTOCK = 5;
+
+        private readonly  List<string> _soda = new List<string>();
+        private readonly List<string> _chips = new List<string>();
+        private readonly List<string> _candy = new List<string>();
+
+        public readonly List<List<string>> Inventory = new List<List<string>>();
 
         
         public string Dispense(string itemToDispense)
         {
-            Inventory.Remove(itemToDispense);
-            return itemToDispense;
+            if (itemToDispense == SODA_STRING)
+            {
+                _soda.Remove(SODA_STRING);
+                return itemToDispense;
+            }
+            else if (itemToDispense == CHIPS_STRING)
+            {
+                _chips.Remove(CHIPS_STRING);
+                return itemToDispense;
+            }
+            else if (itemToDispense == CANDY_STRING)
+            {
+                _candy.Remove(CANDY_STRING);
+                return itemToDispense;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public void Restock()
         {
-            Inventory.Add("Soda");
+            for (int numAdded = 0; numAdded < NUM_ITEMS_ADDED_IN_RESTOCK; numAdded++)
+            {
+                _soda.Add(SODA_STRING);
+                _chips.Add(CHIPS_STRING);
+                _candy.Add(CANDY_STRING);
+            }
+            
+            Inventory.Add(_soda);
+            Inventory.Add(_chips);
+            Inventory.Add(_candy);
         }
     }
 }
--- VendingMachine/VendingMachineTests/VmFoodDispenserTest.cs
+++ VendingMachine/VendingMachineTests/VmFoodDispenserTest.cs
@@ -14,7 +14,10 @@ namespace VendingMachineTests
             VmFoodDispenser testFoodDispenser = new VmFoodDispenser();
             Assert.AreEqual(0,testFoodDispenser.Inventory.Count);
             testFoodDispenser.Restock();
-            Assert.AreEqual(1,testFoodDispenser.Inventory.Count);
+            Assert.AreEqual(3,testFoodDispenser.Inventory.Count);
+            Assert.AreEqual(5,testFoodDispenser.Inventory[0].Count);
+            Assert.AreEqual(5, testFoodDispenser.Inventory[1].Count);
+            Assert.AreEqual(5, testFoodDispenser.Inventory[2].Count);
         }
 
         [TestMethod]
@@ -22,9 +25,21 @@ namespace VendingMachineTests
         {
             VmFoodDispenser testFoodDispenser = new VmFoodDispenser();
             testFoodDispenser.Restock();
-            Assert.AreEqual(1, testFoodDispenser.Inventory.Count);
+            Assert.AreEqual(5, testFoodDispenser.Inventory[0].Count);
             testFoodDispenser.Dispense("Soda");
-            Assert.AreEqual(0, testFoodDispenser.Inventory.Count);
+            Assert.AreEqual(4, testFoodDispenser.Inventory[0].Count);
+        }
+
+        [TestMethod]
+        public void TestOtherItemsAreNotRemovedFromInventoryWhenDispensed()
+        {
+            VmFoodDispenser testFoodDispenser = new VmFoodDispenser();
+            testFoodDispenser.Restock();
+            Assert.AreEqual(5, testFoodDispenser.Inventory[0].Count);
+            testFoodDispenser.Dispense("Soda");
+            Assert.AreEqual(4, testFoodDispenser.Inventory[0].Count);
+            Assert.AreEqual(5, testFoodDispenser.Inventory[1].Count);
+            Assert.AreEqual(5, testFoodDispenser.Inventory[2].Count);
         }
 
         [TestMethod]
@@ -34,6 +49,24 @@ namespace VendingMachineTests
             string itemToDispense = "Soda";
             string dispensedItem = testFoodDispenser.Dispense(itemToDispense);
             Assert.AreEqual("Soda", dispensedItem);
+        }
+
+        [TestMethod]
+        public void TestFoodDispenserCanDispenseChips()
+        {
+            VmFoodDispenser testFoodDispenser = new VmFoodDispenser();
+            string itemToDispense = "Chips";
+            string dispensedItem = testFoodDispenser.Dispense(itemToDispense);
+            Assert.AreEqual("Chips", dispensedItem);
+        }
+
+        [TestMethod]
+        public void TestFoodDispenserCanDispenseCandy()
+        {
+            VmFoodDispenser testFoodDispenser = new VmFoodDispenser();
+            string itemToDispense = "Candy";
+            string dispensedItem = testFoodDispenser.Dispense(itemToDispense);
+            Assert.AreEqual("Candy", dispensedItem);
         }
     }
 }
