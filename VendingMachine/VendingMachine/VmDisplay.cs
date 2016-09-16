1fd9ce7b586ebab78762147a64d6cec561c3493e
jhawke <jeff.hawke@outlook.com>
2016-09-16 19:24:24 -0400

    starting tests for the display

---
+++ VendingMachine/VendingMachine/VmDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VendingMachine
+{
+    public class VmDisplay
+    {
+        private string _displayString;
+        private readonly VmCoinValidator _validator;
+
+        public VmDisplay(VmCoinValidator validator)
+        {
+            _validator = validator;
+        }
+
+        private void SetDisplay()
+        {
+            /*           if (_validator.GetCurrentTransactionTotal() > 0)
+                       {
+                           stringToDisplay = _validator.GetCurrentTransactionTotal().ToString();
+                       }*/
+
+            _displayString = "INSERT COIN";
+        }
+
+        public string CheckDisplay()
+        {
+            SetDisplay();
+            return _displayString;
+        }
+    }
+}
--- VendingMachine/VendingMachineTests/VmDisplayTest.cs
+++ VendingMachine/VendingMachineTests/VmDisplayTest.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachine;
 
 namespace VendingMachineTests
 {
     [TestClass]
     public class VmDisplayTest
     {
-        [TestMethod]
-        public void TestDisplayShowsInsertCoin()
+        private VmFoodDispenser _foodDispenser;
+        private VmCoinReturn _coinReturn;
+        private VmCoinValidator _validator;
+        private VmFoodSlot _foodSlot;
+        private VmCoinSlot _coinSlot;
+        private VmCoinBank _coinBank;
+        private VmDisplay _testDisplay;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            _testDisplay = new VmDisplay(_validator);
+            _foodDispenser = new VmFoodDispenser();
+            _coinReturn = new VmCoinReturn();
+            _validator = new VmCoinValidator();
+            _coinSlot = new VmCoinSlot(new List<string>(), _coinReturn, _validator);
+            _foodSlot = new VmFoodSlot();
+            _coinBank = new VmCoinBank(new List<string>(), _validator, _coinSlot, _coinReturn);
         }
+
+
+        [TestMethod]
+        public void TestDisplayShowsInsertCoinByDefault()
+        {
+           Assert.AreEqual("INSERT COIN" , _testDisplay.CheckDisplay());
+        }
+
     }
 }
